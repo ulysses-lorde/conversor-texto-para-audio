186d571c4affc8b75728cf1c037fdb30709cb649
ulysses-lorde <ulysses.oliveira2015@gmail.com>
2023-08-30 12:43:27 -0300

    programa para reproducao de audio em ambiente desktop usando pygame

--- reprodutor_pygame.py
+++ reprodutor_pygame.py
@@ -1,22 +1,20 @@
 '''Reproduzir arquivo de áudio usando pygame'''
-import os
-
 import pygame
 
 
-def carregar_audio(audio_file):
-    '''Carrega o arquivo de áudio'''
-    # Inicializa o pygame e mixer
-    pygame.init()
-    pygame.mixer.init()
+class AudioPlayer:
+    '''Classe com funções relacionadas
+    à reprodução de áudio usando o pygame'''
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
 
-    # Inicia a reprodução do áudio
-    pygame.mixer.music.load(audio_file)
-    pygame.mixer.music.play()
-    # Aguarda até que a reprodução termine
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-
-    # Fecha o pygame
-    pygame.quit()
-    os.remove(audio_file)
+    def play_audio(self, audio_file):
+        pygame.mixer.music.load(audio_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    
+    def close(self):
+        '''Finaliza o pygame'''
+        pygame.quit()
