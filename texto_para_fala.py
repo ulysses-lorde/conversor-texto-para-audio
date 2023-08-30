6b84683f322945183355f400e8ebb486c5b9efad
ulysses-lorde <ulysses.oliveira2015@gmail.com>
2023-08-30 12:55:07 -0300

    att programa conversor, incompleto v1.11

--- texto_para_fala.py
+++ texto_para_fala.py
@@ -1,18 +1,13 @@
 '''Converter texto em fala em Python'''
-#import os
 import time
 
 import subprocess
 from gtts import gTTS
 
-
-# Conta os números por unidade
-MY_TEXT = 'Passe um texto como argumento'
-
-def gerar_arquivo_audio(my_text=MY_TEXT, language='pt-br'):
+def gerar_arquivo_audio(language='pt-br'):
     '''Gera arquivo de áudio em português'''
-    my_obj = gTTS(text=my_text, lang=language, slow=False)
-    my_obj.save('test.mp3')
+    my_obj = gTTS(text='oi', lang=language, slow=False)
+    my_obj.save('static/test.mp3')
 
 def reproduzir_audio(arquivo_audio='test.mp3'):
     '''Reproduz e apaga o arquivo em seguida'''
